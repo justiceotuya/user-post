c3ff2624ea4b3131f4452eaaab17b8e43e3893c1
Justice Otuya <jotuya2@gmail.com>
2025-07-23 22:09:57 +0100

    feat: add DefaultCatchBoundary, LoadingComponent, and NotFound components for improved error handling and loading states

--- frontend/src/components/loading-component.tsx
+++ frontend/src/components/loading-component.tsx
@@ -1,13 +1,13 @@
 
-const UsersLoadingComponent = () => {
+const LoadingComponent = ({ title }: { title: string }) => {
     return (
         <main>
             <div className='max-w-[855px] mx-auto mt-10 lg:mt-32 px-4'>
-                <h1 className="text-6xl font-medium text-gray-900 mb-6">Users</h1>
+                <h1 className="text-6xl font-medium text-gray-900 mb-6">{title}</h1>
                 <div className='rounded-xl bg-white p-4 mt-4 border border-gray-200 h-[300px]'>
                     <div className="w-full flex flex-col items-center justify-center h-full">
                         <div>
-                            <p className="text-gray-600">Loading users...</p>
+                            <p className="text-gray-600">Loading {title.toLowerCase()}...</p>
                         </div>
                     </div>
                 </div>
@@ -16,4 +16,4 @@ const UsersLoadingComponent = () => {
     )
 }
 
-export default UsersLoadingComponent
+export default LoadingComponent
