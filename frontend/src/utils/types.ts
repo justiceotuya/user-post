9738e829da5baecc2186a090ea0ff1adcd0ee957
Justice Otuya <jotuya2@gmail.com>
2025-07-23 22:11:32 +0100

    feat: add types for User, TPagination, UserWithAddress, UsersResponse, PostType, and PostsResponse

---
+++ frontend/src/utils/types.ts
@@ -0,0 +1,47 @@
+export type User = {
+  id: number
+  name: string
+  email: string
+}
+
+export type TPagination = {
+currentPage:number
+endIndex:number
+hasNextPage:boolean
+hasPreviousPage:boolean
+itemsOnCurrentPage:number
+limit:number
+nextPage:number | null
+offset:number
+previousPage:null | number
+startIndex:number
+totalCount:number
+totalPages:number
+}
+export type UserWithAddress = User & {
+  address: {
+    street: string
+    city: string
+    state: string
+    zipcode: string
+  }
+}
+
+export type UsersResponse = {
+  data: UserWithAddress[]
+  pagination: TPagination
+}
+
+export type PostType = {
+  id: string;
+  title: string;
+    body: string;
+    created_at: string;
+    user: string;
+    email: string;
+}
+
+export type PostsResponse = {
+  data: PostType[]
+  pagination: TPagination
+}
